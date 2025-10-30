cdec1b9a14566f9241d0dc6cec7628b03dacfe50
MaryumShakeel <48406@students.riphah.edu.pk>
2025-10-30 18:09:44 +0500

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,17 @@
 module.exports = {
   testEnvironment: "node",
+  testMatch: ["**/tests/**/*.test.js"],
   reporters: [
     "default",
     [
       "jest-junit",
-      { outputDirectory: "./test-results", outputName: "results.xml" }
+      {
+        outputFile: "junit.xml",   // ✅ Jenkins will read this
+        suiteName: "Jest Tests",
+        ancestorSeparator: " › ",
+        usePathForSuiteName: "true"
+      }
     ]
-  ]
+  ],
+  verbose: true
 };
